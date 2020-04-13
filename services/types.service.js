a65f706e32419799a2931f41d50d895afe8999dd
Manuel <ManuelDiazGil96@hotmail.com>
2020-04-13 13:48:02 +0200

    adding promises

--- services/types.service.js
+++ services/types.service.js
@@ -1,10 +1,12 @@
 
 exports.check_date = function(fields){
-    let check = false;
-    for(let index in fields){
-        if (fields[index].type == 'date'){
-            check = true;
+    return new Promise((resolve,reject) => {
+        let check = false;
+        for(let index in fields){
+            if (fields[index].type == 'date'){
+                check = true;
+            }
         }
-    }
-    return check;
+        resolve(check);
+    });
 }
--- webhooks/conversationals.js
+++ webhooks/conversationals.js
@@ -11,13 +11,15 @@ module.exports = function(app){
 
     app.intent('Decision Model - Relationship', conv => {
         request(elastic_url + 'covid_canada',{json: true}, function(err,res,body) {
-          //console.log(body.covid_canada.mappings.properties) // 200
-          if(types.check_date(body.covid_canada.mappings.properties)) {
-            conv.contexts.set(AppContexts.OVERTIME,5);
-          } else {
-            conv.contexts.set(AppContexts.NO_OVERTIME,5);
-          }
-          conv.ask('¿Que tipo de relación?');
+          console.log(body.covid_canada.mappings.properties) // 200
+          types.check_date(body.covid_canada.mappings.properties).then((res,err) => {
+            if(res) {
+                conv.contexts.set(AppContexts.OVERTIME,5);
+              } else {
+                conv.contexts.set(AppContexts.NO_OVERTIME,5);
+              }
+              conv.ask('¿Que tipo de relación?');
+          });
         });
     })
 }
